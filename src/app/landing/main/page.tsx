8f240b315acf517e102b640835bf81bd4edc88bf
saullope <saul.lopez688@std.uni.edu.ni>
2024-07-17 14:37:35 -0600

    Prueba de integracion

---
+++ src/app/landing/main/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello St-Mary-App Page</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Image from "next/image";
+import { redirect } from "next/navigation"
 
 export default function Home() {
-  return (
-    <>
-    st-mary-school project
-    </>
-  );
+  redirect('landing/main')
 }
