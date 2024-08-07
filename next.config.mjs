026c5ceb9215c01de74982b5440da1b29440a6d0
saullope <saul.lopez688@std.uni.edu.ni>
2024-08-06 21:05:22 -0600

    Agregando funcionalidad de cambio de idioma

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    distDir: 'build',
     output: 'standalone',
   };
   
--- src/components/LocaleSwitcherSelect.tsx
+++ src/components/LocaleSwitcherSelect.tsx
@@ -28,36 +28,44 @@ export default function LocaleSwitcherSelect({
 
 
 return (
-    <div className="me-4">
-        <div className="position-relative flex items-center"> {/* Add the flex and items-center classes here */}
-            <select
-                defaultValue={defaultValue}
-                onChange={(e) => onChange(e.target.value)}
-                className={clsx(
-                    `form-select rounded-sm transition-colors hover:bg-slate-200 cursor-pointer`,
-                    isPending && 'pointer-events-none opacity-60'
-                )}
-                style={{
-                    WebkitAppearance: 'none',
-                    MozAppearance: 'none',
-                    appearance: 'none',
-                    background: 'none',
-                    paddingRight: '1.5rem',
-                    fontSize: '0.875rem',
-                    cursor:'pointer'
-                }}
-            >
-                {items.map((item) => (
-                    <option
-                        key={item.value}
-                        className="d-flex cursor-default align-items-center px-3 py-2 text-base data-[highlighted]:bg-slate-100"
-                        value={item.value}
-                    >
-                            {item.label}
-                    </option>
-                ))}
-            </select>
-        </div>
+  <div className="me-4">
+    <div className="position-relative flex items-center">
+      <select
+        defaultValue={defaultValue}
+        onChange={(e) => onChange(e.target.value)}
+        className={clsx(
+          `form-select rounded-sm transition-colors hover:bg-slate-200 cursor-pointer`,
+          isPending && 'pointer-events-none opacity-60'
+        )}
+        style={{
+          WebkitAppearance: 'none',
+          MozAppearance: 'none',
+          appearance: 'none',
+          background: 'none',
+          paddingRight: '1.5rem',
+          fontSize: '0.875rem',
+          cursor:'pointer'
+        }}
+      >
+        {items.map((item) => (
+          <option
+            key={item.value}
+            className="d-flex cursor-default align-items-center px-3 py-2 text-base data-[highlighted]:bg-slate-100"
+            value={item.value}
+            style={{
+              appearance: 'none',
+              padding: '0.5rem', 
+              borderRadius: 0,
+              border: '1px solid',
+              backgroundPosition: 'right 0.75rem center',
+              backgroundSize: '8px 10px'
+            }}
+          >
+            {item.label}
+          </option>
+        ))}
+      </select>
     </div>
+  </div>
 );
 }
